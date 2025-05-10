3efd9700cdcd816e8884b694a20f1bbe421f280a
Jady Wright <iamjwright159@gmail.com>
2025-05-09 23:32:57 -0400

    Add connector dragging/dropping

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,6 @@ fn main() {
                 update_side_panel.run_if(resource_changed::<StateTypes>),
                 update_nodes,
                 draw_noodle,
-                drag_connector,
             ),
         )
         .add_systems(Update, quit_on_esc)
@@ -180,6 +179,29 @@ fn update_nodes(
     for (node, state) in nodes.iter() {
         commands.entity(node).despawn_related::<Children>();
 
+        let _enter_connector = commands
+            .spawn((
+                Node {
+                    width: Val::Px(15.0),
+                    height: Val::Px(15.0),
+                    border: UiRect::all(Val::Px(3.0)),
+                    position_type: PositionType::Absolute,
+                    left: Val::Px(-20.0),
+                    ..default()
+                },
+                BackgroundColor(css::WHITE.into()),
+                BorderRadius::all(Val::Percent(100.0)),
+                BorderColor(css::BLACK.into()),
+                Connector::Enter,
+                Button,
+                ChildOf(node),
+            ))
+            .observe(start_dragging_connector)
+            .observe(be_dragging_connector)
+            .observe(drag_and_drop_connector)
+            .observe(stop_dragging_connector)
+            .id();
+
         for (state_name, state_value) in state
             .0
             .iter()
@@ -222,10 +244,14 @@ fn update_nodes(
                     }),
                     BorderRadius::all(Val::Percent(100.0)),
                     BorderColor(css::BLACK.into()),
-                    Connector,
+                    Connector::Exit,
                     Button,
                     ChildOf(connector_anchor),
                 ))
+                .observe(start_dragging_connector)
+                .observe(be_dragging_connector)
+                .observe(drag_and_drop_connector)
+                .observe(stop_dragging_connector)
                 .id();
 
             commands
@@ -242,37 +268,55 @@ fn update_nodes(
     }
 }
 
-enum NoodleEnd {
-    Hanging(Vec2),
-    Connector(Entity),
+#[derive(Component)]
+enum Noodle {
+    Connected {
+        start_connector: Entity,
+        end_connector: Entity,
+    },
+    HangingStart {
+        start_position: Vec2,
+        end_connector: Entity,
+    },
+    HangingEnd {
+        start_connector: Entity,
+        end_position: Vec2,
+    },
 }
 
 #[derive(Component)]
-struct Noodle {
-    start: NoodleEnd,
-    end: NoodleEnd,
+enum Connector {
+    Enter,
+    Exit,
 }
-
-#[derive(Component)]
-struct Connector;
 
 fn draw_noodle(
     noodles: Query<&Noodle>,
-    node_ends: Query<&GlobalTransform>,
+    connectors: Query<&GlobalTransform, With<Connector>>,
     window: Query<&Window>,
     mut gizmos: Gizmos,
 ) -> Result {
     let window = window.single()?;
     for noodle in noodles.iter() {
-        let start = (match noodle.start {
-            NoodleEnd::Hanging(pos) => pos,
-            NoodleEnd::Connector(entity) => node_ends.get(entity)?.translation().xy(),
+        let start = (match noodle {
+            Noodle::Connected {
+                start_connector, ..
+            } => connectors.get(*start_connector)?.translation().xy(),
+            Noodle::HangingStart { start_position, .. } => *start_position,
+            Noodle::HangingEnd {
+                start_connector, ..
+            } => connectors.get(*start_connector)?.translation().xy(),
         } - window.size() / 2.0)
             * Vec2::new(1.0, -1.0);
 
-        let end = (match noodle.end {
-            NoodleEnd::Hanging(pos) => pos,
-            NoodleEnd::Connector(entity) => node_ends.get(entity)?.translation().xy(),
+        let end = (match noodle {
+            Noodle::Connected { end_connector, .. } => {
+                connectors.get(*end_connector)?.translation().xy()
+            }
+            Noodle::HangingStart { end_connector, .. } => {
+                connectors.get(*end_connector)?.translation().xy()
+            }
+            Noodle::HangingEnd { end_position, .. } => *end_position,
         } - window.size() / 2.0)
             * Vec2::new(1.0, -1.0);
 
@@ -292,47 +336,125 @@ fn draw_noodle(
     Ok(())
 }
 
-struct CurrentConnector {
-    connector: Entity,
+#[derive(Component)]
+struct DraggedConnector {
     noodle: Entity,
 }
 
-fn drag_connector(
-    connectors: Query<(Entity, &Interaction), With<Connector>>,
-    mut noodles: Query<&mut Noodle>,
+fn start_dragging_connector(
+    trigger: Trigger<Pointer<DragStart>>,
+    connectors: Query<&Connector>,
     mut commands: Commands,
-    mut current_connector: Local<Option<CurrentConnector>>,
     window: Query<&Window>,
 ) -> Result {
     let window = window.single()?;
-    if let Some(CurrentConnector { connector, noodle }) = *current_connector {
-        let (_connector, interaction) = connectors.get(connector)?;
-        if !matches!(interaction, Interaction::Pressed) {
-            commands.entity(noodle).despawn();
-            *current_connector = None;
-        } else {
-            let mut noodle = noodles.get_mut(noodle)?;
-            noodle.end = match noodle.end {
-                NoodleEnd::Hanging(pos) => {
-                    NoodleEnd::Hanging(window.cursor_position().unwrap_or(pos))
-                }
-                _ => unreachable!(),
-            };
-        }
-    } else {
-        for (connector, interaction) in connectors.iter() {
-            if matches!(interaction, Interaction::Pressed) {
-                let noodle = commands
-                    .spawn(Noodle {
-                        start: NoodleEnd::Connector(connector),
-                        end: NoodleEnd::Hanging(window.cursor_position().unwrap_or_default()),
-                    })
-                    .id();
-                *current_connector = Some(CurrentConnector { connector, noodle });
-                return Ok(());
-            }
-        }
+    let connector = trigger.target();
+    let noodle = match connectors.get(connector)? {
+        Connector::Enter => commands
+            .spawn((Noodle::HangingStart {
+                start_position: window.cursor_position().unwrap_or_default(),
+                end_connector: connector,
+            },))
+            .id(),
+        Connector::Exit => commands
+            .spawn((Noodle::HangingEnd {
+                start_connector: connector,
+                end_position: window.cursor_position().unwrap_or_default(),
+            },))
+            .id(),
+    };
+    commands
+        .entity(connector)
+        .insert(DraggedConnector { noodle });
+    Ok(())
+}
+
+fn be_dragging_connector(
+    trigger: Trigger<Pointer<Drag>>,
+    dragged_connectors: Query<&DraggedConnector>,
+    mut noodles: Query<&mut Noodle>,
+    window: Query<&Window>,
+) -> Result {
+    let connector = trigger.target();
+    let noodle = dragged_connectors.get(connector)?.noodle;
+    let mut noodle = noodles.get_mut(noodle)?;
+    let window = window.single()?;
+    *noodle = match *noodle {
+        Noodle::HangingStart { start_position, .. } => Noodle::HangingStart {
+            start_position: window.cursor_position().unwrap_or(start_position),
+            end_connector: connector,
+        },
+        Noodle::HangingEnd { end_position, .. } => Noodle::HangingEnd {
+            start_connector: connector,
+            end_position: window.cursor_position().unwrap_or(end_position),
+        },
+        _ => unreachable!(),
+    };
+    Ok(())
+}
+
+fn drag_and_drop_connector(
+    trigger: Trigger<Pointer<DragDrop>>,
+    mut commands: Commands,
+    connectors: Query<&Connector>,
+    dragged_connectors: Query<&DraggedConnector>,
+    mut noodles: Query<&mut Noodle>,
+) -> Result {
+    let connector = trigger.dropped;
+    let new_connector = trigger.target();
+    let new_connector_type = connectors.get(new_connector)?;
+    let noodle = dragged_connectors.get(connector)?.noodle;
+    let noodle_type = noodles.get(noodle)?;
+
+    if matches!(
+        (noodle_type, new_connector_type),
+        (Noodle::HangingEnd { .. }, Connector::Exit)
+            | (Noodle::HangingStart { .. }, Connector::Enter)
+    ) {
+        println!("Noodle connected to wrong side, removing");
+        return Ok(());
     }
+
+    if noodles.iter().any(|noodle| {
+        matches!(&noodle, Noodle::Connected { start_connector, end_connector }
+			if (*start_connector == new_connector && *end_connector == connector)
+			|| (*start_connector == connector && *end_connector == new_connector))
+    }) {
+        println!("Noodle already exists, removing");
+        return Ok(());
+    }
+
+    println!("Connecting noodle");
+    let mut noodle = noodles.get_mut(noodle)?;
+    *noodle = match *noodle {
+        Noodle::HangingStart { .. } => Noodle::Connected {
+            start_connector: new_connector,
+            end_connector: connector,
+        },
+        Noodle::HangingEnd { .. } => Noodle::Connected {
+            start_connector: connector,
+            end_connector: new_connector,
+        },
+        _ => unreachable!(),
+    };
+    commands.entity(connector).remove::<DraggedConnector>();
+
+    Ok(())
+}
+
+fn stop_dragging_connector(
+    trigger: Trigger<Pointer<DragEnd>>,
+    mut commands: Commands,
+    dragged_connectors: Query<&DraggedConnector>,
+) -> Result {
+    let connector = trigger.target();
+    let noodle = match dragged_connectors.get(connector) {
+        Ok(dragged) => dragged.noodle,
+        Err(_) => return Ok(()), // Could've been handled by [`drag_and_drop_connector`]
+    };
+    println!("Dropping noodle");
+    commands.entity(noodle).despawn();
+    commands.entity(connector).remove::<DraggedConnector>();
 
     Ok(())
 }
